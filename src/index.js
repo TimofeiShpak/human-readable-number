4df7d9a9843289b5460fb2a2f66ff6a3d553b550
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2021-02-01 10:56:08 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,59 +1,57 @@
-//cd C:\human-readable-number
-module.exports = function toReadable (number) {
-  let values = {
-  	1 : 'one',
-  	2 : 'two',
-  	3 : 'three',
-  	4 : 'four',
-  	5 : 'five',
-  	6 : 'six',
-  	7 : 'seven',
-  	8 : 'eight',
-  	9 : 'nine',
-  	10 : 'ten',
-  	11 : 'eleven',
-  	12 : 'twelve',
-  	13 : 'thirteen',
-  	14 : 'fourteen',
-  	15 : 'fifteen',
-  	16 : 'sixteen',
-  	17 : 'seventeen',
-  	18 : 'eighteen',
-  	19 : 'nineteen',
-  	20 : 'twenty',
-  	30 : 'thirty',
-  	40 : 'forty',
-  	50 : 'fifty',
-  	60 : 'sixty',
-  	70 : 'seventy',
-  	80 : 'eighty',
-  	90 : 'ninety',
-  	0 : ''
-  }
-  let str = '';
-  let units = 0;
-  let tens = 0;
-  let hundreds = 0;
-  if(number==0)return 'zero';
-  if(number<=20) str = values[number];
-  if(number>20 && number<=100) {
-	units = number%10;
-  	tens = number-units;
-  	if(number%10!=0)str = values[tens]+' '+values[units];
-  	else str = values[tens];
-  }
-  if(number==100)return 'one hundred';
-  if(number>100){
-	units = number%10; 
-  	hundreds = number-number%100;
-  	tens = number - hundreds - units;
-  	hundreds/=100;
-  	if(tens==0 && units ==0){str = values[hundreds]+' hundred';return str;}
-  	if(tens==10){units=number%100;str = values[hundreds]+' hundred '+values[units];return str;}
-  	if(tens==0){str = values[hundreds]+' hundred '+values[units];return str;}
-  	if(number%10==0 && tens!=10){str = values[hundreds]+' hundred '+values[tens];return str;}
-  	if(number%10!=0 && tens!=10){str = values[hundreds]+' hundred '+values[tens]+' '+values[units];return str;}
-  }
-  return str;
+numbers = {
+    '0' : 'zero',
+    '1' : 'one',
+    '2' : 'two',
+    '3' : 'three',
+    '4' : 'four',
+    '5' : 'five',
+    '6' : 'six',
+    '7' : 'seven',
+    '8' : 'eight',
+    '9' : 'nine',
+    '10' : 'ten',
+    '11' : 'eleven',
+    '12' : 'twelve',
+    '13' : 'thirteen',
+    '14' : 'fourteen',
+    '15' : 'fifteen',
+    '16' : 'sixteen',
+    '17' : 'seventeen',
+    '18' : 'eighteen',
+    '19' : 'nineteen',
+    '20' : 'twenty',
+    '30' : 'thirty',
+    '40' : 'forty',
+    '50' : 'fifty',
+    '60' : 'sixty',
+    '70' : 'seventy',
+    '80' : 'eighty',
+    '90' : 'ninety',
+    '100' : 'hundread',
 }
 
+module.exports = function toReadable (number) {
+    let result = '';
+    let hundreds = 0;
+    let ten = 0;
+    if (number > 99) {
+        hundreds = (number - number % 100) / 100;
+        number -= hundreds*100;
+        result += numbers[`${hundreds}`]+' '+'hundred'
+    }
+    if (number > 19) {
+      if (hundreds) {
+          result += ' ';
+      }
+      ten = (number - number % 10);
+      number -= ten;
+      result += numbers[`${ten}`];
+    }
+    if (number % 100 < 20 && number % 100 > 0) {
+        if (ten || hundreds) {
+            result += ' ';
+        }
+        result += numbers[`${number % 100}`];
+    }
+    return result || 'zero';
+  }
